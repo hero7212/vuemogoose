4b167cd4516cd40d86ebff0c355e8c80daa1ca33
wenshuai <3054750147@qq.com>
2018-10-14 17:22:55 +0800

    a

---
+++ src/pages/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <el-row></el-row>
+    <el-table
+      ref="multipleTable"
+      :data="list"
+      tooltip-effect="dark"
+      style="width: 100%">
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
+      <el-table-column
+        prop="_id"
+        label="ID">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="age"
+        label="年龄">
+      </el-table-column>
+    </el-table>
+
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      list: [],
+    }
+  },
+  methods: {
+    async getListAsync() {
+      const {data} = await axios.get('api/getList')
+      this.list = data;
+    }
+  },
+  created() {
+    this.getListAsync()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
